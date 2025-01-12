a371762f9d43c86f24bf1f3fa705e7a55360b5a4
carlodelos90 <carlodelos90@gmail.com>
2025-01-12 17:59:11 +0100

    work in progress

---
+++ wwwroot/yourTurn.js
@@ -0,0 +1,76 @@
+import { playerlist } from './usernamePreGame.js'
+
+// antal spelare kommer från annan data
+//deras data kan vara "player" och antal
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log(playerlist);
+
+    });
+    // const listActivePlayer= ["Patrik", "Edvin", "Kasper", "Carlo", "Albin", "Hugo"];
+    // function shuffledPlayers() {
+    // listActivePlayer.sort((a, b) => 0.5 - Math.random())
+    //     console.log(listActivePlayer)
+    // }
+    // shuffledPlayers()
+
+
+
+
+
+
+
+
+
+
+
+//TRUE RANDOM 
+/*
+let addPlayerToArray = 0
+let players = 0
+const playerTurnArray= [];
+
+
+var numbersOfPlayers = listActivePlayer.length
+var randomPlayer = Math.floor(Math.random()*numbersOfPlayers)
+
+playerTurnArray.push(randomPlayer)
+//listActivePlayer[randomPlayer].splice(playerTurnArray)
+
+//var item = listActivePlayer[Math.floor(Math.random()*listActivePlayer.length)];
+
+console.log(randomPlayer)
+console.log([playerTurnArray])
+
+while(players <= currentPlayers){
+    players +=
+        
+addPlayerToArray = player1
+addPlayerToArray = player2
+addPlayerToArray = player3
+    
+    
+        listActivePlayer.push(addPlayerToArray)
+}
+
+
+
+const shuffleArray = listActivePlayer => {
+    for (let i = listActivePlayer.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i * 1));
+        const temp = listActivePlayer[i];
+        listActivePlayer[i] = listActivePlayer[j]
+        listActivePlayer[j] = temp;
+
+    }
+}
+console.log(listActivePlayer)
+
+let player1 = "Edvin"
+let player2 = "Patrik"
+let player3 = "Kasper"
+
+*/
