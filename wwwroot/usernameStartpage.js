c5e3e0257c1de95a7917cf8d578686d9e74a05e0
carlodelos90 <carlodelos90@gmail.com>
2025-02-02 11:09:53 +0100

    username limiter

--- wwwroot/usernameStartpage.js
+++ wwwroot/usernameStartpage.js
@@ -2,47 +2,47 @@ document.addEventListener("DOMContentLoaded", () => {
   const acceptButton = document.getElementById("acceptButton");
   const playerNameInput = document.getElementById("playerNameInput");
   const errorMessage = document.getElementById("errorMessage");
-  
-  let playerCount = parseInt(localStorage.getItem('playerCount')) || 0;
-  
+
+  let playerCount = parseInt(localStorage.getItem("playerCount")) || 0;
 
   acceptButton.addEventListener("click", () => {
     const username = playerNameInput.value.trim();
-  
+
     if (username === "") {
       errorMessage.style.display = "block";
       errorMessage.textContent = "Please enter a username.";
       return;
     }
-  
+
     playerCount++;
-    localStorage.setItem('playerCount', playerCount);
-  
+    localStorage.setItem("playerCount", playerCount);
+
     errorMessage.style.display = "none";
 
     addPlayerName().then(() => {
-    window.location.href = "joinGame.html";
+      window.location.href = "joinGame.html";
     });
   });
-  
-  //#region Carlo 
+
+  //#region Carlo
   async function addPlayerName() {
     const playerName = $('[id="playerNameInput"]').val();
-    console.log('PlayerName:', playerName);
+    console.log("PlayerName:", playerName);
     try {
-      const response = await fetch('/new-player/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ word: playerName })
+      const response = await fetch("/new-player/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ word: playerName }),
       });
-  
-      console.log('response', response);
+
+      console.log("response", response);
       const data = await response.json();
-      console.log('data', data);
-      $('#message').text(playerName + ' lades till i databasen');
+      console.log("data", data);
+      $("#message").text(playerName + " lades till i databasen");
     } catch (error) {
-      console.error('Error adding player:', error);
+      console.error("Error adding player:", error);
     }
   }
   //#endregion
 });
+
